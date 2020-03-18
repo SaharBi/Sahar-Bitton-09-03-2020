8f67530511824d1bd16357a8ac53b7c5b7d80942
Sahar Biton <saharbi@walla.co.il>
2020-03-18 12:50:54 +0200

    Updates

---
+++ precache-manifest.e30bbd71a876944a5c8a585a325b638a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a99ce5f69592fed0d719f5e9067f6fb5",
+    "url": "/sahar-bitton-09-03-2020/index.html"
+  },
+  {
+    "revision": "178763ac8cad2484a550",
+    "url": "/sahar-bitton-09-03-2020/static/css/2.d3b247d0.chunk.css"
+  },
+  {
+    "revision": "c58b391d45d1e0bcd3d7",
+    "url": "/sahar-bitton-09-03-2020/static/css/main.08f2223c.chunk.css"
+  },
+  {
+    "revision": "178763ac8cad2484a550",
+    "url": "/sahar-bitton-09-03-2020/static/js/2.3f26e8eb.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/sahar-bitton-09-03-2020/static/js/2.3f26e8eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c58b391d45d1e0bcd3d7",
+    "url": "/sahar-bitton-09-03-2020/static/js/main.5d3fa57a.chunk.js"
+  },
+  {
+    "revision": "80354f24e679255ac56d",
+    "url": "/sahar-bitton-09-03-2020/static/js/runtime-main.a4e10097.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/sahar-bitton-09-03-2020/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/sahar-bitton-09-03-2020/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/sahar-bitton-09-03-2020/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/sahar-bitton-09-03-2020/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/sahar-bitton-09-03-2020/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
